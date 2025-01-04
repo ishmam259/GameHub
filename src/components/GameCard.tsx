1d925be03d5671f9ee31c3cd5dcb1473ffba28ed
ishmam259 <tahmid12955@gmail.com>
2025-01-04 12:06:48 +0600

    6th

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,23 +1,27 @@
-import { Game } from "../hooks/useGames";
-import { CardBody } from "@chakra-ui/react";
-import { Heading } from "@chakra-ui/react";
-import { Image,Card } from "@chakra-ui/react";
-import { Text } from "@chakra-ui/react";
-import PlatformIconList from "./PlatformIconList";
+import { Card, CardBody, Heading, HStack, Image} from '@chakra-ui/react'
+
+import { Game } from '../hooks/useGames'
+import PlatformIconList from './PlatformIconList'
+import CriticScore from './CriticScore'
 
 interface Props {
-  game: Game;
+  game: Game
 }
+
 const GameCard = ({ game }: Props) => {
   return (
     <Card borderRadius={10} overflow='hidden'>
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize='2xl'>{game.name}</Heading>
-        {game.parent_platforms.map(({platform}) => <Text>{platform.name}</Text>)}
+        <HStack justifyContent='space-between'>
+        <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)} />
+        <CriticScore score={game.metacritic}/>
+        </HStack>
       </CardBody>
     </Card>
-  );
-};
+  )
+}
 
-export default GameCard;
+export default GameCard
+
